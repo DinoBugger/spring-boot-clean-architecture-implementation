b4e9eefe82f4fabc7e6c170f21317416834d7426
PhanVanManh <phanvanmanh3936@gmail.com>
2026-04-22 15:30:53 +0700

    initialize project

---
+++ src/main/java/com/dinobugger/cleanproject/CleanprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dinobugger.cleanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CleanprojectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dinobugger/cleanproject/CleanprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinobugger.cleanproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanprojectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
